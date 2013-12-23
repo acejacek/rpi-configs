83ec918f744aea377b42ed7ebaec233983af7882
acejacek <acejacek@gmail.com>
2013-12-23 19:46:34 +0100

    Update prep.sh

--- bin/prep.sh
+++ bin/prep.sh
@@ -12,7 +12,7 @@
 
 
 #REPOSITORY="git@github.com:meinside/raspiconfigs.git"
-REPOSITORY="git://github.com/acejacek/raspiconfigs.git"
+REPOSITORY="git://github.com/acejacek/rpi-configs.git"
 TMP_DIR="$HOME/configs.tmp"
 
 echo -e "\033[32mThis script will setup several things for your Raspberry Pi\033[0m\n"
